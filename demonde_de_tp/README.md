23109b78baf23d555ae7ac5f7a84fd423a631255
unknown <zarkik.mohamed@etu.uae.ac.ma>
2026-04-05 04:58:14 +0100

    my version

---
+++ demonde_de_tp/README.md
@@ -0,0 +1,23 @@
+1 Create a folder 
+2 cd your folder 
+3 run "python -m venv monenv" 
+4 run "monenv\Scripts\activate"
+5 Clone the project
+run "git clone https://github.com/fatimazahrafatima/Projet-de-fin-de-module-Python.git"
+6 run "cd Projet-de-fin-de-module-Python"
+7 run "pip install -r requirements.txt"
+8 run"python manage.py runserver" wiel you are in Projet-de-fin-de-module
+9 go to http://127.0.0.1:8000/
+
+Test Accounts
+Admin
+email: admin@gamil.om
+Password: 11
+
+Teacher
+email: samira@gmail.com
+Password: 11
+
+Student
+email: benkiran@gmail.com	
+Password: 11
--- student/urls.py
+++ student/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
- path('students/', views.student_list, name='students_teacher'),
- path('dashboard/', views.student_dashboard, name='student_dashboard'),
- path('add/', views.add_student, name='add_student'),
- path('<str:student_id>/',views.view_student, name='view_student'),
- path('edit/<str:student_id>/',views.edit_student, name='edit_student'),
- path('delete/<str:student_id>/',views.delete_student, name='delete_student'),
- path('departments/', views.departmentsve, name='departmentsur'),
- path('Courses/', views.Courses_ve, name='Courses_ur'),
- path('Exams/', views.Exam_ve, name='Exam_ur'),
- path('grades/', views.grades_ve, name='grades_ur'),
+    path('students/', views.student_list, name='students_teacher'),
+    path('dashboard/', views.student_dashboard, name='student_dashboard'),
+    path('add/', views.add_student, name='add_student'),
+
+    path('departments/', views.departmentsve, name='departmentsur'),
+    path('Courses/', views.Courses_ve, name='Courses_ur'),
+    path('Exams/', views.Exam_ve, name='Exam_ur'),
+    path('grades/', views.grades_ve, name='grades_ur'),
+
+    path('edit/<str:student_id>/', views.edit_student, name='edit_student'),
+    path('delete/<str:student_id>/', views.delete_student, name='delete_student'),
+    path('<str:student_id>/', views.view_student, name='view_student'),
 ]
